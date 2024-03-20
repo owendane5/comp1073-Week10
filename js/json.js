4699c503288b8625c5e5e6319f46ab80b5f065e9
owend <owendane5@gmail.com>
2024-03-20 11:25:50 -0400

    started lab03

--- js/json.js
+++ js/json.js
@@ -7,7 +7,7 @@ async function populate(){
     
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
-    const requestURL = "https://smccrindle.github.io/comp1073module10/js/i-scream.json";    
+    const requestURL = "https://owendane5.github.io/comp1073-Week10/js/i-scream.json";    
     // STEP 5: Use the new URL to create a new request object
     const request = new Request(requestURL);
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
